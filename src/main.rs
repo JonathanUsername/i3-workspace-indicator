1581c97028709d388879160dbbef35c3c35d0a1d
jonathanusername <jonathan.j.king@gmail.com>
2020-11-13 13:51:29 +0000

    Refactor to be a subscription, more efficient, and order correctly

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,53 @@
 extern crate i3ipc;
+
 use i3ipc::I3Connection;
+use i3ipc::I3EventListener;
+
+use i3ipc::event::Event;
+use i3ipc::reply::Workspace;
+use i3ipc::Subscription;
+
+use i3ipc::event::inner::WorkspaceChange;
 
 const UNFOCUSED: &'static str = "%{F#6F6F6F}%{F-}";
 const FOCUSED: &'static str = "";
 
-fn main() {
-    // establish a connection to i3 over a unix socket
-    let mut connection = I3Connection::connect().unwrap();
-    
-    let workspaces = connection.get_workspaces().unwrap().workspaces;
-    let bools: Vec<bool> = workspaces.iter().map(|i| i.focused).collect();
+// const UNFOCUSED: &'static str = "NO";
+// const FOCUSED: &'static str = "YES";
 
-    let out_str: Vec<String> = bools.iter().map(|&i| {
-        let val = if i { FOCUSED } else { UNFOCUSED };
-        return val.to_owned();
-    }).collect();
-    println!("{}", out_str.join(" "));
+fn get_ordered_workspaces() -> Vec<Workspace> {
+    let mut connection = I3Connection::connect().unwrap();
+    let mut workspaces = connection.get_workspaces().unwrap().workspaces;
+    workspaces.sort_by_key(|i| i.num);
+    workspaces
 }
 
+fn get_symbolised_workspace_focus() -> String {
+    let workspaces = get_ordered_workspaces();
+    let max_workspace_num = workspaces.iter().map(|i| i.num).max().unwrap();
+
+    (1..=max_workspace_num).map(|idx| {
+        match workspaces.iter().find(|w| w.num == idx) {
+            Some(w) => if w.focused { FOCUSED } else { UNFOCUSED},
+            None => UNFOCUSED
+        }
+    }).collect::<Vec<&str>>().join(" ")
+}
+
+fn main() {
+    // establish connection.
+    let mut listener = I3EventListener::connect().unwrap();
+
+    let subs = [Subscription::Workspace];
+    listener.subscribe(&subs).unwrap();
+
+    for event in listener.listen() {
+        match event.unwrap() {
+            Event::WorkspaceEvent(e) => match e.change {
+                WorkspaceChange::Focus => println!("{}", get_symbolised_workspace_focus()),
+                _ => (),
+            },
+            _ => (),
+        }
+    }
+}
